a74bdf2c6021ee3c1e138ac0ddf217ec0da62661
gzhon <gzhongtian845@gmail.com>
2022-08-26 11:48:40 +0900

    競プロテンプレート

---
+++ temp.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+using let=long long;
+using P = pair<int,int>;
+using vecs = vector<string>;
+using vec1 = vector<int>;
+using vec2 = vector<vector<int>>;
+using Graph = vector<vector<int>>;
+
+vec1 dx={1,0,-1,0};
+vec1 dy={0,1,0,-1};
+int main(void)
+{
+     
+    return 0;  
+}
