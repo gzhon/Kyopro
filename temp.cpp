0f0bb8860a42db45ae16385115062ac0f7d67464
gzhon <104480880+gzhon@users.noreply.github.com>
2022-08-28 16:16:56 +0900

    dx8とdy8の追加(斜めも含む)

--- temp.cpp
+++ temp.cpp
@@ -2,14 +2,16 @@
 #define isin(i,j,h,w) (!((i<0||i>=h||j<0||j>=w))?true:false)
 using namespace std;
 using let=long long;
-using P = pair<int,int>;
+using P = pair<let,let>;
 using vecs = vector<string>;
-using vec1 = vector<int>;
-using vec2 = vector<vector<int>>;
-using Graph = vector<vector<int>>;
+using vec1 = vector<let>;
+using vec2 = vector<vector<let>>;
+using Graph = vector<vector<let>>;
 
-vec1 dx={1,0,-1,0};
-vec1 dy={0,1,0,-1};
+vec1 dx4={1,0,-1,0};
+vec1 dy4={0,1,0,-1};
+vec1 dx8={1,1,1,0,0,-1,-1,-1};
+vec1 dy8={1,0,-1,1,-1,1,0,-1};
 int main(void)
 {
      
