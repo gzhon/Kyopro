23000c76ff45dc08dc8da61f4832d5e07b23572b
KEI <104480880+gzhon@users.noreply.github.com>
2022-11-05 12:53:41 +0900

    Add Union Find

--- temp.cpp
+++ temp.cpp
@@ -12,6 +12,37 @@ vec1 dx4={1,0,-1,0};
 vec1 dy4={0,1,0,-1};
 vec1 dx8={1,1,1,0,0,-1,-1,-1};
 vec1 dy8={1,0,-1,1,-1,1,0,-1};
+struct UnionFind
+{   
+    vec1 par,size;
+    UnionFind(int n):par(n,-1),size(n,1) {
+    }
+    //根
+    int root(int q){
+        if(par[q]==-1) return q;
+        else {
+            return par[q]=root(par[q]);
+        }
+    }
+    //A,Bは同一か?
+    bool issame(int a,int b){
+        return root(a)==root(b);
+    }
+    //Aを含むやつとBを含むやつくっっける
+    bool unite(int a,int b){
+        a=root(a);
+        b=root(b);
+        if(a==b) return false;
+        if(size[a]<size[b]){
+            swap(a,b);
+        }
+        par[b]=a;
+        size[a]+=size[b];
+        return true;
+    }
+    //Qを含むやつの大きさ
+    int w_size(int q){ return size[root(q)]; }
+};
 int main(void)
 {
      
